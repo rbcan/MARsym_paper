9528d97ddbb325f56154b5e969d37a6debbb2189
Rebecca <35726619+rbcan@users.noreply.github.com>
2018-09-06 13:50:31 +0200

    Update geneHaplotyping.viquas1.3.sh

--- geneHaplotyping.viquas1.3.sh
+++ geneHaplotyping.viquas1.3.sh
@@ -12,33 +12,36 @@ DATE=`date +%d%m%Y`
 echo "Your logfile is geneHaplotyping."$DATE".log" 
 echo =================================================================
 
+ref="<ref>" # Replace <ref> with reference name for which you have ref.fasta
+sample="s1" # this example runs with sample s1, replace with your corresponding sample
+
 cd /PATH/ViQuaS1.3/ # this folder contains the ViQuaS program parts - important to run script in that folder
 
 mkdir ./fasta.files/
 mkdir ./bam.files/
 
-# preparation file sample.viquasIn for all samples s1, s2, s3, ... sn: Contig <tab> geneStart <tab> geneEnd; e.g. Node_15  500 1500
-# this example runs with sample s1
+# preparation file $sample.viquasIn for all samples s1, s2, s3, ... sn: Contig <tab> geneStart <tab> geneEnd; e.g. Node_15  500 1500
 
-gene_pos=$(awk -F "\t" '{print $2":"$3"-"$4}' s1.viquasIn | tr '\n' '\t')
+
+gene_pos=$(awk -F "\t" '{print $2":"$3"-"$4}' $sample.viquasIn | tr '\n' '\t')
 
 for gene in $gene_pos
 do
 samtools faidx $ref.fasta $gene > ./fasta.files/$ref.$gene.fa
-samtools view -b /PATH/100x.s1.bam $gene > ./bam.files/s1.$gene.bam # input from mapping required either use downsampled or realigned bam-file
-samtools index ./bam.files/s1.$gene.bam # creates a bam-file for each gene
-samtools view -h -F 4 -b ./bam.files/s1.$gene.bam > ./bam.files/s1.$gene.onlymapped.bam # keeps only the gene region
-samtools index ./bam.files/s1.$gene.onlymapped.bam
-rm ./bam.files/s1.$gene.bam
+samtools view -b /PATH/100x.$sample.bam $gene > ./bam.files/$sample.$gene.bam # input from mapping required either use downsampled or realigned bam-file
+samtools index ./bam.files/$sample.$gene.bam # creates a bam-file for each gene
+samtools view -h -F 4 -b ./bam.files/$sample.$gene.bam > ./bam.files/$sample.$gene.onlymapped.bam # keeps only the gene region
+samtools index ./bam.files/$sample.$gene.onlymapped.bam
+rm ./bam.files/$sample.$gene.bam
 done
 
 for gene in $gene_pos
 do
 calc=$(echo $gene | sed 's/.*://g')
 a=$(echo $((-1*($calc)))) # calculates gene length
-timeout 5m Rscript ./ViQuaS.R ./fasta.files/$ref.$gene.fa ./bam.files/s1.$gene.onlymapped.bam 5 0.7 1 $a
-mv ViQuaS-Richness.txt Richness.s1.$gene.txt
-mv ViQuaS-Spectrum.fa Spectrum.s1.$gene.fa
+timeout 5m Rscript ./ViQuaS.R ./fasta.files/$ref.$gene.fa ./bam.files/$sample.$gene.onlymapped.bam 5 0.7 1 $a
+mv ViQuaS-Richness.txt Richness.$sample.$gene.txt
+mv ViQuaS-Spectrum.fa Spectrum.$sample.$gene.fa
 rm -r SIM
 rm -r Samples
 rm -r viquas
